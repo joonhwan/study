794fcf93a0aa07ebc7a6f593b9470197ad8db30b
Joonhwan <joonhwan.lee@gmail.com>
2013-03-15 17:39:28 +0900

    learn up to chap 19 half

---
+++ dotnet/ProCsharp/chap19/ThreadTest.cs
@@ -0,0 +1,45 @@
+// Local Variables:
+// compile-command:"csc /target:exe								\
+//   /r:System.dll,System.Windows.Forms.dll,System.Drawing.dll	\
+//   /main:ThreadTest /out:ThreadTest.exe ThreadTest.cs			\
+//   && ThreadTest arg1 "arg2""
+// End:
+using System;
+using System.Threading;
+
+public class MyWorker
+{
+	private int m_count;
+	public MyWorker()
+		: this (10)
+	{
+	}
+	public MyWorker(int count)
+	{
+		m_count = count;
+	}
+	public void PrintNumbers()
+	{
+		for (int i = 0; i < m_count; ++i)
+		{
+			Random r = new Random();
+			var number = r.Next(5);
+			// Console.Write("number {0} : ", number);
+			Thread.Sleep(100 * r.Next(5));
+			Console.Write("{0}, ", i);
+		}
+		Console.WriteLine();
+	}	
+}
+
+public class ThreadTest
+{
+	public static void Main()
+	{
+		MyWorker worker = new MyWorker(20);
+		Thread myThread = new Thread(worker.PrintNumbers);
+		myThread.Start();
+		// PrintNumbers();
+		Console.WriteLine("End.");
+	}
+}
